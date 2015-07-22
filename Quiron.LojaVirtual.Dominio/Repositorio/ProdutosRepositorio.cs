3941c2d6c5b429fb02f50c23aacf5ad04b87799f
sidneigs <sidnei.gs@gmail.com>
2015-07-22 19:00:30 -0300

    Aula 6

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly EFDBContext _contexto = new EFDBContext();
+
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _contexto.Produtos; }
+        }
+
+    }
+}
